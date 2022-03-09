a5a1ea2d845e854375e6c3296e4db1e4d4897275
meaguilar <meaguilar@uca.edu.sv>
2022-03-09 17:33:31 -0600

    :seedling: PrimerProyecto-CP1

---
+++ CP1/CP1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CP1
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+    //Escribe tu programa un seleccionador de dias de la semana, o de equipo
+    
+    }
+  }
+}
